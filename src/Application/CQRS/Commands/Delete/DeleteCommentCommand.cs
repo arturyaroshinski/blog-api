70494dd592c545029c5a5560d7e5c725ecb24111
Artur Yaroshinski <62154380+arturyaroshinski@users.noreply.github.com>
2021-04-01 21:40:11 +0300

    feat: add comments controller and comands/queries

---
+++ src/Application/CQRS/Commands/Delete/DeleteCommentCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Yaroshinski.Blog.Application.Exceptions;
+using Yaroshinski.Blog.Application.Interfaces;
+using Yaroshinski.Blog.Domain.Entities;
+
+namespace Yaroshinski.Blog.Application.CQRS.Commands.Delete
+{
+    public class DeleteCommentCommand : IRequest
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+    
+    public class DeleteCommentCommandHandler : IRequestHandler<DeleteCommentCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteCommentCommandHandler(IApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        
+        public async Task<Unit> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Comments.FindAsync(request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Comment), request.Id);
+            }
+
+            _context.Comments.Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Application/CQRS/Queries/Get/GetCommentsQuery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Yaroshinski.Blog.Application.DTO;
+using Yaroshinski.Blog.Application.Interfaces;
+
+namespace Yaroshinski.Blog.Application.CQRS.Queries.Get
+{
+    public class GetCommentsQuery : IRequest<List<CommentDto>>
+    {
+        // TODO: add page size
+        [Required]
+        public int PostId { get; set; }
+    }
+    
+    public class GetCommentsQueryHandler : IRequestHandler<GetCommentsQuery, List<CommentDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetCommentsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        
+        public async Task<List<CommentDto>> Handle(GetCommentsQuery request, CancellationToken cancellationToken)
+        {
+            var comments = await _context
+                .Comments
+                .Where(c => c.PostId == request.PostId)
+                .ToListAsync(cancellationToken: cancellationToken);
+
+            return _mapper.Map<List<CommentDto>>(comments);
+        }
+    }
+}
